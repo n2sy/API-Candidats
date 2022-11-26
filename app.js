27af3009dc5522d4ab6eb2627c3e2459c962dc65
nidhal <nidhal.jelassi@fsegt.utm.tn>
2022-11-26 17:21:34 +0100

    add vercel

--- app.js
+++ app.js
@@ -33,12 +33,6 @@ mongoose.connect(`mongodb+srv://${process.env.username}:${process.env.password}@
     useUnifiedTopology: true
 }).then(result => app.listen(process.env.PORT || 3000, console.log("Server started !")))
     .catch(err => console.log(err));
-// mongoose.connect('mongodb://localhost:27017/Laposte3', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }).then(result => app.listen(3000, console.log("Server started !")))
-//     .catch(err => console.log(err));
-
 
 
 
