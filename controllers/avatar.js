81e47656730101c6106d505ae6730abff920b705
n2sy <nidhal.jelassi@fsegt.utm.tn>
2024-04-04 18:35:46 +0100

    first commit

--- controllers/avatar.js
+++ controllers/avatar.js
@@ -1,9 +1,8 @@
 exports.postAvatar = (req, res, next) => {
-    //console.log(filter);
-    console.log(req.file);
-    let fileName = req.file.originalname; //To save in DB 
-    res.status(200).json({
-        'message': 'Image uploaded successfully',
-        'fileName': fileName
-    });
-}
+  console.log(req.file);
+  let fileName = req.file.filename; //To save in DB
+  res.status(200).json({
+    message: "Image uploaded successfully",
+    fileName: fileName,
+  });
+};
